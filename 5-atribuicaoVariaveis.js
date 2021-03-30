5a7c7455d61b694560da074f946217bcfcbbbffd
João Victor <jjoaovictorpg@gmail.com>
2021-03-30 14:25:03 -0300

    Add aula 5

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 18;
+const nome = "João";
+const sobrenome = "Gomes";
+
+// Formas para colocar espaço entre variáveis
+console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+
+ console.log(`Meu nome é ${nome} ${sobrenome}`);

ef1be81b1bac5f6bc6a2d0ac9e62ed3707648ec1
João Victor <jjoaovictorpg@gmail.com>
2021-03-30 12:09:32 -0300

    Atualizando arquivo

--- 2-TrabalhandoComVariaveis.js
+++ 2-TrabalhandoComVariaveis.js
@@ -1,2 +1,11 @@
 // Console.log("Trabalhando com variáveis");
 console.log("Trabalhando com variáveis");
+
+// JS é case sensitive
+
+const idade = 29;
+const nome = "João";
+
+// Não fazer
+
+mes = "Janeiro"; // Sempre use o var, const ou let;
